7a8378c93005ab1afbc7022fc0275651f41b5865
eszclie <eszclie@gmail.com>
2024-10-03 16:32:32 +0200

    Update 1_Prediction.py

--- app/user_interface/1_Prediction.py
+++ app/user_interface/1_Prediction.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-import src.etl_neerslag.functions.get_average as avg
+import get_average as avg
 from app.user_interface.funcs import bind_socket, get_response, set_page_confic
 
 # Set the name and icon of the page
